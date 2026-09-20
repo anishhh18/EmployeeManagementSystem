799bbbc55f97cb39c3cd3649272a03fdc2645b2f
anishhh18 <anish990990@gmail.com>
2026-09-20 17:42:15 +0530

    Added login based page rendering

--- EMS-FRONTEND/src/App.jsx
+++ EMS-FRONTEND/src/App.jsx
@@ -1,19 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Login from "./components/Auth/Login"
 import AdminDashboard from "./components/Dashboard/AdminDashboard"
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
 import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage"
 
 function App() {
- useEffect(()=>{
-  // setLocalStorage()
-  getLocalStorage()
- })
+    const [user,setUser] = useState(null)
+    const handleLogin = (email,password)=>{
+      if(email == "admin@gmail.com" && password == "123"){
+        setUser("admin")
+      }else if(email == "rahul@gmail.com" && password == "123"){
+        setUser("employee")
+      }else{
+        alert("Invaild credentials")
+      }
+    }
   return (
       <>
-      <Login/>
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard/> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard/> : <EmployeeDashboard/> }
       </>
   )
 }
